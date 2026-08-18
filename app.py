a64bf6307c91df134075ceccea8936e02f72f74d
supriyok92 <supriyo.karmakar92@gmail.com>
2026-08-18 11:49:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from pathlib import Path
 st.set_page_config(
     page_title="ABC Alphabet Explorer",
     page_icon="🔤",
-    layout="centered",
+    layout="wide",
     initial_sidebar_state="collapsed",
 )
 
